00ed2aa80b35343b51db152d42705da3ec5c778d
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-07-13 15:22:11 -0300

    Add: elimino la funcion para eliminar operaciones en el libro contable

--- src/routes/statistics/LibroContable.js
+++ src/routes/statistics/LibroContable.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
 import MainNavBar from '../orders/MainNavBar';
 import SERVER from '../server'
 
@@ -22,7 +21,6 @@ function Statistics() {
     const [fechaFinSearch, setFechaFinSearch] = useState("");
 
     const branchId = JSON.parse(localStorage.getItem("branchId"))
-    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchStates = async () => {
@@ -110,6 +108,21 @@ function Statistics() {
         setIdSelectMovement(id);
       }
     };
+    
+    async function handleDelete(id) {
+      try {
+        await axios.delete(`${SERVER}/movname/${id}`)
+          .then(response => {
+            alert("Operacion eliminada")
+            window.location.reload()
+          })
+          .catch(error => {
+              console.error(error)
+          })
+      } catch (error) {
+        alert(error)
+      }
+    }
 
     // Obtener las filas correspondientes a la página actual
     const paginatedRows = paginateData();
@@ -120,73 +133,75 @@ function Statistics() {
             <div className="bg-white my-2 px-2 max-w-7xl mx-auto">
               <div className='text-center'>
                 <h1 className="text-5xl font-bold py-8">Libro contable</h1>
+                {/* Buscador de registros */}
                 <div className="border border-gray-300">
-                        <form onSubmit={handleSearch} className="p-1 bg-blue-100">
-                            <div className='grid grid-cols-3 gap-y-1'>
-                                <div className='flex justify-end w-5/6 gap-x-2'>
-                                    <label>Ingreso</label>
-                                    <input
-                                        className="text-end w-52"
-                                        type="text"
-                                        value={ingresoSearch}
-                                        onChange={(e) => setIngresoSearch(e.target.value)}
-                                    />
-                                </div>
-                                <div className='flex justify-end w-5/6 gap-x-2'>
-                                    <label>Fecha Inicio </label>
-                                    <input
-                                        className='w-52'
-                                        type="date"
-                                        value={fechaInicioSearch}
-                                        onChange={(e) => setFechaInicioSearch(e.target.value)}
-                                    />
-                                </div>
-                                <div className='flex justify-end w-5/6 gap-x-2'>
-                                    <label>Fecha Fin </label>
-                                    <input
-                                        className='w-52'
-                                        type="date"
-                                        value={fechaFinSearch}
-                                        onChange={(e) => setFechaFinSearch(e.target.value)}
-                                    />
-                                </div>
-                                <div className='flex justify-end w-5/6 gap-x-2'>
-                                    <label>Egreso </label>
-                                    <input
-                                        className='w-52'
-                                        type="text"
-                                        value={egresoSearch}
-                                        onChange={(e) => setEgresoSearch(e.target.value)}
-                                    />
-                                </div>
-                                <div className='flex justify-end w-5/6 gap-x-2'>
-                                    <label>Monto mínimo </label>
-                                    <input
-                                        className='w-52'
-                                        type="text"
-                                        value={montoMinSearch}
-                                        onChange={(e) => setMontoMinSearch(e.target.value)}
-                                    />
-                                </div>        
-                                <div className='flex justify-end w-5/6 gap-x-2'>
-                                    <label>Monto máximo </label>
-                                    <input
-                                        className='w-52'
-                                        type="text"
-                                        value={montoMaxSearch}
-                                        onChange={(e) => setMontoMaxSearch(e.target.value)}
-                                    />
-                                </div>                     
+                    <form onSubmit={handleSearch} className="p-1 bg-blue-100">
+                        <div className='grid grid-cols-3 gap-y-1'>
+                            <div className='flex justify-end w-5/6 gap-x-2'>
+                                <label>Ingreso</label>
+                                <input
+                                    className="text-end w-52"
+                                    type="text"
+                                    value={ingresoSearch}
+                                    onChange={(e) => setIngresoSearch(e.target.value)}
+                                />
                             </div>
-                            <div className='flex justify-end'>
-                                <button
-                                    type='submit'
-                                    className="px-1 text-black font-bold bg-white rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-200" >
-                                    Buscar
-                                </button>
+                            <div className='flex justify-end w-5/6 gap-x-2'>
+                                <label>Fecha Inicio </label>
+                                <input
+                                    className='w-52'
+                                    type="date"
+                                    value={fechaInicioSearch}
+                                    onChange={(e) => setFechaInicioSearch(e.target.value)}
+                                />
                             </div>
-                        </form>
-                    </div>
+                            <div className='flex justify-end w-5/6 gap-x-2'>
+                                <label>Fecha Fin </label>
+                                <input
+                                    className='w-52'
+                                    type="date"
+                                    value={fechaFinSearch}
+                                    onChange={(e) => setFechaFinSearch(e.target.value)}
+                                />
+                            </div>
+                            <div className='flex justify-end w-5/6 gap-x-2'>
+                                <label>Egreso </label>
+                                <input
+                                    className='w-52'
+                                    type="text"
+                                    value={egresoSearch}
+                                    onChange={(e) => setEgresoSearch(e.target.value)}
+                                />
+                            </div>
+                            <div className='flex justify-end w-5/6 gap-x-2'>
+                                <label>Monto mínimo </label>
+                                <input
+                                    className='w-52'
+                                    type="text"
+                                    value={montoMinSearch}
+                                    onChange={(e) => setMontoMinSearch(e.target.value)}
+                                />
+                            </div>        
+                            <div className='flex justify-end w-5/6 gap-x-2'>
+                                <label>Monto máximo </label>
+                                <input
+                                    className='w-52'
+                                    type="text"
+                                    value={montoMaxSearch}
+                                    onChange={(e) => setMontoMaxSearch(e.target.value)}
+                                />
+                            </div>                     
+                        </div>
+                        <div className='flex justify-end'>
+                            <button
+                                type='submit'
+                                className="px-1 text-black font-bold bg-white rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-200" >
+                                Buscar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                {/* Tabla con registros */}
                 <table className="mt-4 w-full">
                   <thead>
                     <tr>
@@ -196,7 +211,6 @@ function Statistics() {
                       <th className="px-4 py-2 border border-black">Egreso</th>
                       <th className="px-4 py-2 border border-black">Monto</th>
                       <th className="px-4 py-2 border border-black">Usuario</th>
-                      <th className="px-4 py-2 border border-black">Editar</th>
                       <th className="px-4 py-2 border border-black">Eliminar</th>
                     </tr>
                   </thead>
@@ -215,15 +229,7 @@ function Statistics() {
                           <td className="px-4 py-2">{row.username}</td>
                           <td className="px-4 py-2">
                             <button
-                            onClick={() => {navigate('/home')}}
-                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
-                            >
-                              Editar
-                            </button>
-                          </td>
-                          <td className="px-4 py-2">
-                            <button
-                            onClick={() => {navigate('/home')}}
+                            onClick={() => handleDelete(row.idmovname)}
                             className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
                             >
                               Eliminar
