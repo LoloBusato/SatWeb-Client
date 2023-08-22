fe559c9f3cc040915d966448324d3bf363228b40
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-08-22 12:15:32 -0300

    Modify: ajusto los estilos de la pagina de gastos para telefonos

--- src/routes/finances/movements.js
+++ src/routes/finances/movements.js
@@ -10,9 +10,9 @@ function Movements() {
             <MainNavBar />
             <div className='bg-white my-2 py-8 px-2 max-w-7xl mx-auto'>
                 <h1 className="text-center text-5xl">Gastos</h1>
-                <div className='flex flex-wrap mt-10'>
+                <div className='flex flex-wrap justify-center mt-10'>
                     {/* Otros */}
-                    <div className="w-1/2 mb-10 flex justify-center">
+                    <div className="mb-10 flex sm:w-1/2 justify-center">
                         <button
                             className="bg-blue-500 text-2xl hover:bg-blue-700 text-white font-bold py-4 px-8 rounded"
                             onClick={() => navigate('/movesothers')}>
@@ -20,7 +20,7 @@ function Movements() {
                         </button>
                     </div>
                     {/* Ventas */}
-                    <div className="w-1/2 mb-10 flex justify-center">
+                    <div className="mb-10 flex sm:w-1/2 justify-center">
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded text-2xl"
                             onClick={() => navigate('/movessells')}>
@@ -28,7 +28,7 @@ function Movements() {
                         </button>
                     </div>
                     {/* Sucursales */}
-                    <div className="w-1/2 mb-10 flex justify-center">
+                    <div className="mb-10 flex sm:w-1/2 justify-center">
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded text-2xl"
                             onClick={() => navigate('/movesbranches')}>
@@ -36,7 +36,7 @@ function Movements() {
                         </button>
                     </div>
                     {/* Inyeccion de capitales */}
-                    <div className="w-1/2 mb-10 flex justify-center">
+                    <div className="mb-10 flex sm:w-1/2 justify-center">
                         <button
                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded text-2xl"
                             onClick={() => navigate('/movescapital')}>
