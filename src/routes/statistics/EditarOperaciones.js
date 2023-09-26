39cbcdda349e20f07bcf990f70e5faa985fc5e02
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-09-26 12:27:05 -0300

    Fix: arreglo un error que no dejaba actualizar las operaciones en editarOperaciones

--- src/routes/statistics/EditarOperaciones.js
+++ src/routes/statistics/EditarOperaciones.js
@@ -8,6 +8,7 @@ function EditarOperaciones() {
 
     const [selectMovname, setSelectMovname] = useState([])
     const [selectCobro, setSelectCobro] = useState([])
+    const [selectMovement, setSelectMovement] = useState([])
 
     const [encargadoId, setEncargadoId] = useState(0)
     const [pesosId, setPesosId] = useState(0)
@@ -18,6 +19,7 @@ function EditarOperaciones() {
     const branchId = JSON.parse(localStorage.getItem("branchId"))
 
     const [dolar, setDolar] = useState(800)
+    const [isNotLoading, setIsNotLoading] = useState(true);
 
     const location = useLocation();
     const movnameId = parseInt(location.pathname.split("/")[2]);
@@ -34,15 +36,25 @@ function EditarOperaciones() {
                     console.error(error)
                 })
 
+            await axios.get(`${SERVER}/movements/${branchId}`)
+                .then(response => {
+                    const allMovements = response.data
+                    const filteredMovements = allMovements.filter((item) => item.movname_id === movnameId)
+                    setSelectMovement(filteredMovements)
+                    filteredMovements.forEach(element => {
+                        if (document.getElementById(element.categories) !== null) {
+                            document.getElementById(element.categories).value = parseFloat(element.unidades)
+                        }
+                    });
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+
             await axios.get(`${SERVER}/cobros/movname/${movnameId}`)
                 .then(response => {
                     const valoresCobro = response.data[0]
                     setSelectCobro(valoresCobro)
-                    document.getElementById('Pesos').value = parseInt(valoresCobro.pesos)
-                    document.getElementById('Dolares').value = parseInt(valoresCobro.dolares)
-                    document.getElementById('Banco').value = parseInt(valoresCobro.banco)
-                    document.getElementById('MercadoPago').value = parseInt(valoresCobro.mercado_pago)
-                    document.getElementById('Encargado').value = parseInt(valoresCobro.encargado)
                 })
                 .catch(error => {
                     console.error(error)
@@ -80,32 +92,33 @@ function EditarOperaciones() {
     // eslint-disable-next-line
     }, []);
 
-    const [isNotLoading, setIsNotLoading] = useState(true);
-
     async function handleSubmit(event) {
         event.preventDefault();
         if (isNotLoading) {
             try {        
-
                 const pesos = parseInt(document.getElementById('Pesos').value) || 0
                 const usd = parseInt(document.getElementById('Dolares').value) || 0
                 const banco = parseInt(document.getElementById('Banco').value) || 0
                 const mp = parseInt(document.getElementById('MercadoPago').value) || 0
                 const encargado = parseInt(document.getElementById('Encargado').value) || 0
 
-                const mismosValores = (
-                    selectCobro.pesos === pesos &&
-                    selectCobro.dolares === usd &&
-                    selectCobro.banco === banco &&
-                    selectCobro.mercado_pago === mp &&
-                    selectCobro.encargado === encargado
-                )
-                if (mismosValores) {
-                    return alert('Modificar valores')
-                }
-                if (pesos === 0 && usd === 0 && banco === 0 && mp === 0 && encargado === 0) {
-                    setIsNotLoading(true)
-                    return alert("Insertar valores")
+                let cobro = false
+                if (selectCobro) {
+                    cobro = true
+                    const mismosValores = (
+                        selectCobro.pesos === pesos &&
+                        selectCobro.dolares === usd &&
+                        selectCobro.banco === banco &&
+                        selectCobro.mercado_pago === mp &&
+                        selectCobro.encargado === encargado
+                    )
+                    if (mismosValores) {
+                        return alert('Modificar valores')
+                    }
+                    if (pesos === 0 && usd === 0 && banco === 0 && mp === 0 && encargado === 0) {
+                        setIsNotLoading(true)
+                        return alert("Insertar valores")
+                    }
                 }
                 
                 //libro
@@ -141,7 +154,8 @@ function EditarOperaciones() {
                 const responseMovements = await axios.put(`${SERVER}/movements/${movnameId}`, {
                     arrayInsert: arrayMovements,
                     total,
-                    cobrosValues
+                    cobrosValues,
+                    cobro
                 })
                 if (responseMovements.status === 200) {
                     setIsNotLoading(true)
