f4c94398c8ff4c7e53a65131fca5ae8aa6a06bab
Lorenzo Busato <lorenzobusato07@gmail.com>
2024-04-11 09:05:56 -0300

    fix: fix update where you could not write negative values

--- src/routes/statistics/EditarOperaciones.js
+++ src/routes/statistics/EditarOperaciones.js
@@ -134,7 +134,6 @@ function EditarOperaciones() {
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                                     type="number"
                                     step="1" 
-                                    min="0"
                                     id={category.categories}
                                     name={category.categories}
                                 />
