9760d59cbd2da3eb7f4c42a16c30cacea7bf1690
Lorenzo Busato <lorenzobusato07@gmail.com>
2025-02-04 10:24:48 -0300

    update: server.js

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,3 +1,3 @@
-const SERVER = 'https://sat-web-server-9m92s0wgg-lolobusatos-projects.vercel.app/api';
+const SERVER = 'https://sat-web-server-1xtuvhvgp-lolobusatos-projects.vercel.app/api';
 
 export default SERVER;
